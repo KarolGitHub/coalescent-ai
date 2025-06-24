fce1d46ab67b175dd08af1732f51165ab218725c
kgardyjas <karol.gardyjas@zetorzeszow.pl>
2025-06-24 15:40:59 +0200

    feat: client - update eslint configuration

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 // Make sure adding Sentry options is the last code to run before exporting
